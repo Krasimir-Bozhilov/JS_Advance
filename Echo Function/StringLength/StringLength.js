8222b85cc3d6778fa36cf226f083f6f1b469e922
krasi <krasimir.bozhilov@gmail.com>
2021-01-12 09:29:45 +0200

    New

---
+++ Echo Function/StringLength/StringLength.js
@@ -0,0 +1,6 @@
+function StringLength(a,b,c){
+    let sumOfLength=a.length+b.length+c.length;
+    let averageLength=Math.floor(sumOfLength/3);
+    console.log(sumOfLength);
+    console.log(averageLength);
+}
