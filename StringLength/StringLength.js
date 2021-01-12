2a6a19db6c070972b8760e1e49f99da444302a6a
krasi <krasimir.bozhilov@gmail.com>
2021-01-12 09:36:05 +0200

    New

---
+++ StringLength/StringLength.js
@@ -0,0 +1,7 @@
+function stringLength(a,b,c){
+    let sumOfLength=a.length+b.length+c.length;
+    let averageSum=Math.floor(sumOfLength/3);
+    console.log(sumOfLength);
+    console.log(averageSum);
+}
+stringLength('pasta', '5', '22.3')
