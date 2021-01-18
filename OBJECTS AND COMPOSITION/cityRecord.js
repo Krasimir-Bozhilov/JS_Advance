452de9257f98023477e14ff6b750eece85e1b55e
Krasimir-Bozhilov <krasimirbozhilov@gmail.com>
2021-01-18 19:38:31 +0200

    Homework

---
+++ OBJECTS AND COMPOSITION/cityRecord.js
@@ -0,0 +1,10 @@
+function slove(name, population,treasury){
+    let myObj = {name:name,
+    population:population,
+treasury:treasury}
+console.log(myObj);
+return myObj
+}
+slove('Tortuga',
+    7000,
+    15000)
