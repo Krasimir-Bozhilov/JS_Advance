243e8ec18da32b8a94bc83e6fe196eb918d7d1f2
Krasimir-Bozhilov <72642058+Krasimir-Bozhilov@users.noreply.github.com>
2021-01-21 20:48:40 +0200

    Add files via upload

---
+++ DOM/ShowMore.js
@@ -0,0 +1,5 @@
+function showText() {
+    const element = document.getElementById('text');
+    document.getElementById('text').style.display = 'inline'
+    document.getElementById('more').style.display = 'none'
+}
