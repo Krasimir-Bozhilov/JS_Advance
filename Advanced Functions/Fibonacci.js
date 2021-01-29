d737b7f51b4ef55e8510e5124a06740898d6a25b
Krasimir Bozhilov <72642058+Krasimir-Bozhilov@users.noreply.github.com>
2021-01-29 18:40:22 +0200

    Add files via upload

---
+++ Advanced Functions/Fibonacci.js
@@ -0,0 +1,27 @@
+function getFibonacci(){
+let num1=0;
+let num2=1;
+
+return function fib(){
+
+    let result=num1+num2
+    num1=num2;
+    num2=result;
+    return num1
+
+}
+
+
+}
+let fib=getFibonacci()
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
+console.log(fib());
